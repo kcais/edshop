527779b1edbe149c7d0dd1c213bf9daba675d703
kcais <kcais@volny.cz>
2019-11-18 15:36:26 +0100

    Admin rozhrani - vytvoreni nove kategorie prodejnich polozek

--- app/Model/CategoryManager.php
+++ app/Model/CategoryManager.php
@@ -26,4 +26,28 @@ class CategoryManager
             ;
     }
 
+    /** Vytvori novou kategorii prodejnich polozek
+     * @param String $name Nazev kategorie prodejnich polozek
+     * @param String $comment Popis kategorie prodejnich polozek
+     * @param int $order Cislo urcujici poradi kategorie
+     * @param int $parent_cat_id Id nadrazene kategorie
+     * @return int Vraci 1 - pokud se podarilo kategorii vytvorit, 0 - pokud chyba
+     */
+    public function createNewCategory(String $name, String $comment, int $order_id=1, int $parent_cat_id = null) : int
+    {
+        try{
+            $this->database->table("categories")->insert([
+                "name" => $name,
+                "comment" => $comment,
+                "order_id" => $order_id,
+                "parent_cat_id" => $parent_cat_id,
+            ]);
+
+            return 1;
+        }
+        catch(\Exception $e){
+            return 0;
+        }
+    }
+
 }
--- app/Presenters/AdminPresenter.php
+++ app/Presenters/AdminPresenter.php
@@ -2,11 +2,24 @@
 
 namespace App\Presenters;
 
+use App\Model\CategoryManager;
 use Nette;
+use Nette\Application\UI\Form;
 
 
 final class AdminPresenter extends Nette\Application\UI\Presenter
 {
+    private $categoryManager;
+
+    function __construct(CategoryManager $categoryManager)
+    {
+        parent::__construct();
+        $this->categoryManager = $categoryManager;
+    }
+
+    /** Metoda volana pri vytvoreni presenteru, slouzi k overeni zda prihlaseny uzivatel ma admin roli
+     * @throws Nette\Application\AbortException
+     */
     protected function startup()
     {
         //volani metody startup predka
@@ -17,6 +30,40 @@ final class AdminPresenter extends Nette\Application\UI\Presenter
         {
             $this->flashMessage("Přihlášený uživatel nemá roli admin !",'warning');
             $this->redirect("Homepage:");
+        }
+    }
+
+    protected function createComponentAdminCatnewForm() : Form
+    {
+        $form = new Form;
+
+        $form->addText('name','Jméno kategorie :')
+            ->setRequired("Zadejte jméno kategorie");
+
+        $form->addText('comment','Popis kategorie')
+            ->setRequired('Zadejte popis kategorie');
+
+        $form->addText('order','Pořadí kategorie')->setMaxLength(3);
+
+        $form->addText('parent_cat_id','ID nadřazené kategorie');
+
+        $form->addSubmit('create','Vytvořit');
+
+        $form->onSuccess[] = [$this, 'adminCatnewFormSucceeded'];
+
+        return $form;
+    }
+
+    public function adminCatnewFormSucceeded(Form $form, array $values) : void
+    {
+        if(!$values['order'])$values['order']=1;
+        if(!$values['parent_cat_id'])$values['parent_cat_id']=null;
+
+        if($this->categoryManager->createNewCategory($values["name"],$values["comment"],$values["order"], $values["parent_cat_id"])){
+            $this->redirect("Admin:newsuccess");
+        }
+        else{
+            $this->redirect("Admin:newerror");
         }
     }
 }
