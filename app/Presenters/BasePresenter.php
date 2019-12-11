551938b2d252127a17fd0eae41fd1d9de44a3856
kcais <kcais@volny.cz>
2019-12-11 16:02:26 +0100

    Uprava css pro zobrazeni menu

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -19,11 +19,13 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
         $catObjArr = $categoryRepository->findby(['deleted_on' => null],['name' => 'ASC']);
         $category = null;
         $parCategory = null;
+        $childParCat = null;
         foreach ($catObjArr as $catObj){
             $catObj->getParentCat()?$parCatId = $catObj->getParentCat()->getId():$parCatId = null;
 
             if($parCatId){
                 $parCategory[$parCatId][] = $this->em->getCategoryRepository()->find($catObj->getId());
+                $childParCat[$catObj->getId()]=$catObj->getParentCat();
             }
             else {
                 $category[] = ['id' => $catObj->getId(), 'name' => $catObj->getName(), 'description' => $catObj->getDescription(), 'parent_cat_id' => $parCatId];
@@ -34,6 +36,7 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
         $this->template->category2 = $category;
         $this->template->categories = $category;
         $this->template->par_category = $parCategory;
+        $this->template->child_par_category = $childParCat;
     }
 
 }
